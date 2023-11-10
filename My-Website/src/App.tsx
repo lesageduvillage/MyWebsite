b9d2984860c6378bd62d8dc2ff8f83e5eb449a21
Henry Sargerson <91054494+lesageduvillage@users.noreply.github.com>
2023-11-10 09:30:52 +0100

    first commit

---
+++ My-Website/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className={"App"}>
+        <header className={"App-header"}>
+            <div className={"font-bold text-6xl"}>hello world</div>
+        </header>
+    </div>
+  )
+}
+
+export default App
